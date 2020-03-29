28c04686b896baabbbd8fc57f38af7711d311021
Fred C <canofred@gmail.com>
2020-03-29 02:09:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,7 @@ def third_challenge
         :name => "Lady Capulet", :age => "51"
         },
       :heroine => {
-        :name => "Juliet", :age => "15", :status => "alive", :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"}, {:name => "Nurse", :age => "44", :attitude => "worried"}]
+        :name => "Juliet", :age => "15", :status => "alive"}, :heroine_friends => [{:name => "Steven", :age => "30", :attitude => "confused"}, {:name => "Nurse", :age => "44", :attitude => "worried"}]
         },
       
    }
